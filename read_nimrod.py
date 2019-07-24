76923694d0d683b3a28df81427a74ec8c24cf144
simonrp84 <simon.proud@physics.ox.ac.uk>
2019-07-24 11:08:09 +0100

    Add licence informatin

--- read_nimrod.py
+++ read_nimrod.py
@@ -14,6 +14,19 @@
 #   -   where input_file_name is a NIMROD file, uncompressed
 # NOTE: This script has only been tested on 1km composite radar data
 # Bugs: None known
+#
+# LICENSE:
+# This is free software: you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# This is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this.  If not, see <http://www.gnu.org/licenses/>.
 
 
 import sys
